792055c77ae2841ac706a7e028fa19b404f9b403
iHelin <ihelin@outlook.com>
2017-02-15 23:05:36 +0800

    first commit

---
+++ api.js
@@ -0,0 +1,13 @@
+//var protocol = "https";
+//var hostname = "www.ianhe.me";
+//var port = 443;
+//var context = "";
+//var requestPrefix = '';
+
+var protocol = "http";
+var hostname = "192.168.31.252";
+var port = 8080;
+var context = "";
+var requestPrefix = '';
+
+requestPrefix = protocol + '://' + hostname + ':' + port + context + '/';
