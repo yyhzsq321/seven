a5b19e98a8a96292e175500ff1b00b129e9fbc26
iHelin <ihelin@outlook.com>
2017-02-18 11:35:30 +0800

    移除jq，改用Vue； 列表添加下拉刷新

--- api.js
+++ api.js
@@ -1,13 +1,13 @@
-var protocol = "https";
-var hostname = "www.ianhe.me";
-var port = 443;
-var context = "";
-
-
-//var protocol = "http";
-//var hostname = "192.168.31.252";
-//var port = 8080;
+//var protocol = "https";
+//var hostname = "www.ianhe.me";
+//var port = 443;
 //var context = "";
+
+
+var protocol = "http";
+var hostname = "192.168.31.252";
+var port = 8080;
+var context = "";
 
 
 var requestPrefix = '';
